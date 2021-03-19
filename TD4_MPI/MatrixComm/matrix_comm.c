2b1ba0ccd8deee4e25dd5841fe6b58a171751518
Sholde <exshode@gmail.com>
2021-03-19 14:52:51 +0100

    finish exo1 td4 mpi

--- TD4_MPI/MatrixComm/matrix_comm.c
+++ TD4_MPI/MatrixComm/matrix_comm.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     int P, rank;
     int Nib, Njb, ib, jb;
 
-    MPI_Comm comm_row, comm_col;
+    MPI_Comm COMM_ROW, COMM_COL;
 
     MPI_Init(&argc, &argv);
 
@@ -30,6 +30,47 @@ int main(int argc, char **argv)
        A ECRIRE : creation communicateurs comm_row et comm_col
      */
 
+    // Determine index
+    ib = rank / Njb;
+    jb = rank % Njb;
+
+    // Create communicator
+    MPI_Comm_split(MPI_COMM_WORLD, jb, rank, &COMM_ROW);
+    MPI_Comm_split(MPI_COMM_WORLD, ib, rank, &COMM_COL);
+
+    // Recup Nib and Njb in tmp_nib and tmp_njb
+    int tmp_nib = 0;
+    int tmp_njb = 0;
+
+    MPI_Comm_size(COMM_ROW, &tmp_nib);
+    MPI_Comm_size(COMM_COL, &tmp_njb);
+
+    if (Nib != tmp_nib)
+      {
+        printf("nib and tmp_nib are not equal\n");
+      }
+
+    if (Njb != tmp_njb)
+      {
+        printf("njb and tmp_njb are not equal\n");
+      }
+
+    // Recup ib and jb in rank_row and rank_col
+    int rank_row = 0;
+    int rank_col = 0;
+
+    MPI_Comm_rank(COMM_ROW, &rank_row);
+    MPI_Comm_rank(COMM_COL, &rank_col);
+
+    if (ib != rank_row)
+      {
+        printf("ib and rank_row are not equal\n");
+      }
+
+    if (jb != rank_col)
+      {
+        printf("jb and rank_col are not equal\n");
+      }
 
     MPI_Finalize();
 
